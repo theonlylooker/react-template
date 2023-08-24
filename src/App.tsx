d481bf9f44d8960bcb65af4c6a1bdbc86abf6463
basurco eduardo <angelsoulsword@gmail.com>
2023-08-24 16:04:35 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as Logo } from "./assets/react.svg";
+const App = () => {
+  return (
+    <div className="text-4xl">
+      <h1>text</h1>
+      <Logo />
+    </div>
+  );
+};
+
+export default App;
